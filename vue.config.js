8b917c9e1ae4f37eb5ce59f7155f4c4d41921772
Alex Thiong'o Wandugu <119406879+alexpxcell@users.noreply.github.com>
2024-12-20 21:41:09 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/<your-repo-name>/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/AlexWandugu-Portfolio/' // Replace with your repo name
+      : '/',
 }
